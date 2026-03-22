3ecdddbb0df2b41218e7aca5505c6a4481462751
abcs2 <abcs2@cesar.school>
2026-03-22 09:52:29 -0300

    adicionados models e admin

--- pixtar/models.py
+++ pixtar/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
+
+class Imagem(models.Model):
+    titulo = models.CharField(max_length=200, null=False, default='Untitled')
+    autor = models.CharField(max_length=200, null=False, default='Anonymous')
+    data_criacao = models.DateTimeField('Criado em ')
+
+    qtdObjetos = models.IntegerField(default=0)
+    qtdAlfabetos = models.IntegerField(default=0)
+    qtdFontes = models.IntegerField(default=0)
+
+    exposto = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '[' + str(self.id) + '] ' + self.titulo + ' by ' + self.autor
--- project/settings.py
+++ project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pixtar.apps.pixtarConfig',
+    'pixtar.apps.PixtarConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
