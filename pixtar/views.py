6af1e64977fca62def1e468d3d8bfe93e5895e9e
abcs2 <abcs2@cesar.school>
2026-03-22 10:25:06 -0300

    primeiro template adicionado

--- pixtar/views.py
+++ pixtar/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse, Http404
+from .models import Imagem
+
+from django.views import View
+
+from django.utils import timezone
+from django.shortcuts import redirect
+from django.urls import reverse
+
 # Create your views here.
+
+class MainView(View):
+    def get(self, request):
+        ultimas_imagens = Imagem.objects.order_by('-data_criacao')
+        contexto = {'imagens': ultimas_imagens}
+        return render(request, 'pixtar/index.html', contexto)
