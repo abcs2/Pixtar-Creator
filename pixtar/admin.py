58ae1ff12fc38d9570ef73408fc70ba806660c3b
abcs2 <abcs2@cesar.school>
2026-05-16 15:26:48 -0300

    base de dados atualizada

--- pixtar/admin.py
+++ pixtar/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Imagem, sharedImage
+from .models import Imagem, sharedImage, userImage
 
 # Register your models here.
 
 admin.site.register(Imagem)
 admin.site.register(sharedImage)
+admin.site.register(userImage)
